613f08f0dfaba1939dd4c7a06924b73547f599f3
AbdulHakim201103 <abdulhakim201103@gmail.com>
2022-05-24 11:48:10 +0600

    not found page setup

--- src/Shared/NotFound/NotFound.js
+++ src/Shared/NotFound/NotFound.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import notFound from "../../assets/notfound/notFound.webp";
 
 const NotFound = () => {
-    return (
-        <div>
-            <h1>Not Found</h1>
-        </div>
-    );
+  return (
+    <div className="container mx-auto relative">
+      <img className=" w-screen" src={notFound} alt=""></img>
+      <div className="flex  justify-center items-center">
+        <Link className="btn btn-primary " to="/">
+          Go Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
