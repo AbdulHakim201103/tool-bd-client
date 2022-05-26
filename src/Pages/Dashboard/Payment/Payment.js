c53bb5b3f266e79d7764393ba32c04b3e20ccda0
AbdulHakim201103 <abdulhakim201103@gmail.com>
2022-05-26 22:33:47 +0600

    card cade add

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,6 +1,13 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51L3eVgLhVwtYY33gqAYSYbvi7epeNsLBZTa0L1P77N5cQ1SS5IL44vu4MbAjwW0wgh3mMxFPsc3omBjb5EHq3A9l00EfJpLOSj"
+);
 
 const Payment = () => {
   const { id } = useParams();
@@ -36,7 +43,11 @@ const Payment = () => {
         </div>
       </div>
       <div class="card flex-shrink-0 w-50 mx-w-md shadow-2xl bg-base-100">
-        <div class="card-body"></div>
+        <div class="card-body">
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        </div>
       </div>
     </div>
   );
